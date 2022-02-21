b189bc81d5802f8bd6a34a50404dd2d637c707ed
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-02-21 14:48:09 +0100

    output

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let s=0;
-let km = prompt (Inserire il numero di km)
-let age = prompt (Inserire l'età)
+let s = 0;
+let km = prompt ("Inserire il numero di km");
+let age = prompt ("Inserire l'età");
+let p = km * 0.21;
+
+if (age < 18) {
+    s = p * 20 /100;
+} else if (age > 65) {
+    s = p * 40/100;
+}
+
+let prezzoFinale = p - s;
+
+document.getElementById("prezzofinale").innerHTML = "Il prezzo del tuo biglietto è " + prezzoFinale + "€";
